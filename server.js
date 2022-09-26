7c7d1ac5ce1d74b8615824561c9c832a57761e05
Robert Martin <rlmartin1996@gmail.com>
2022-09-26 17:09:13 -0500

    complete besides bonus delete feature

--- server.js
+++ server.js
@@ -5,11 +5,35 @@ const { notes } = require('./data/notes');
 
 const PORT = process.env.PORT || 3005;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
 
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+};
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-  });
+});
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // add note to json file and notes array in this function
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+});
 
 
 
@@ -21,11 +45,19 @@ app.get('/api/notes', (req, res) => {
 
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
+app.delete('api/notes/:id', (req, res) => {
+    const { id } = req.params;
 
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
-  
+});
